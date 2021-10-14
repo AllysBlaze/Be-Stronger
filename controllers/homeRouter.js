2c79f957dd3a2f603c1c0b3ef1192117a1a90876
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2021-10-14 18:06:39 +0200

    home page

--- controllers/homeRouter.js
+++ controllers/homeRouter.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const accessTokenSecret = require('../config')
 const {authenticateRoute,parseJwt}=require('../middleware/authToken')
+const {getUserExtended} =require('../models/user')
 
 
-router.get('/', function (req, res) {
+
+router.get('/', async function (req, res) {
     const username=parseJwt(req.cookies['id']).username;
-    res.send(`Czesc ${username}`)
+    const user_data= await getUserExtended(username)[0]
+    res.send(user_data)
 });
 
 module.exports=router
