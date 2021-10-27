6b9abc131768dc79a9684bfd46b3430c54fe8bda
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2021-10-27 11:36:28 +0200

    beautify files

--- controllers/homeRouter.js
+++ controllers/homeRouter.js
@@ -14,7 +14,9 @@ router.get('/', async function (req, res) {
     const username = parseJwt(req.cookies['id']).username;
     const user_data = await user.getUserExtended(username)
     //res.send(user_data)
-    res.render('profilLayout',{user_name:user_data[0].user_name})
+    res.render('profilLayout', {
+        user_name: user_data[0].user_name
+    })
 });
 
 router.get('/history', async function (req, res) {
@@ -33,25 +35,25 @@ router.get('/progress', async function (req, res) {
     const username = parseJwt(req.cookies['id']).username; //do poprawy
     const id = await user.getUserID(username);
     const data1 = await progress.getTrainingCategories(id[0].user_id);
-    const data2=await progress.getTrainingWeeklyProgress(id[0].user_id);
-    var x1=[];
-    var y1=[];
-    for (var i=0;i<data1.length;i++){
+    const data2 = await progress.getTrainingWeeklyProgress(id[0].user_id);
+    var x1 = [];
+    var y1 = [];
+    for (var i = 0; i < data1.length; i++) {
         x1.push(data1[i].training_category);
         y1.push(data1[i].training_count);
     }
-    var x2=[];
-    var y2=[];
-    for (var i=0;i<data2.length;i++){
+    var x2 = [];
+    var y2 = [];
+    for (var i = 0; i < data2.length; i++) {
         x2.push(data2[i].week);
         y2.push(data2[i].training_count);
     }
 
     res.render('progress', {
-        x1:x1,
-        y1:y1,
-        x2:x2,
-        y2:y2
+        x1: x1,
+        y1: y1,
+        x2: x2,
+        y2: y2
     })
 })
 
