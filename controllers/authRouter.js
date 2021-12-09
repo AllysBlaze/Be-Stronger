a0a83cdedc15c24c9d08ebd77f4f67e7196c1510
AllysBlaze <44466481+AllysBlaze@users.noreply.github.com>
2021-12-09 16:14:53 +0100

    zmiany kosmetyczne

--- controllers/authRouter.js
+++ controllers/authRouter.js
@@ -33,11 +33,11 @@ const login = async (req, res) => {
                 res.redirect('/home')
             }
             res.render('login', {
-                err_msg: 'Nieprawodłowy adres email lub hasło'
+                err_msg: 'Nieprawidłowy adres email lub hasło'
             })
         } else {
             res.render('login', {
-                err_msg: 'Nieprawodłowy adres email lub hasło'
+                err_msg: 'Nieprawidłowy adres email lub hasło'
             })
         }
     } catch (error) {
